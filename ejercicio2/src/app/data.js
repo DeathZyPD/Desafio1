f3b77d4e58643a697df20422227e7f2ec318f6f2
DeathzyPD <pablodagoberto2004@gmail.com>
2024-02-26 12:58:33 -0600

    nuevos cambios

---
+++ ejercicio2/src/app/data.js
@@ -0,0 +1,76 @@
+export const data = [
+    {
+        id: 1,
+        title: 'Conjunto para yoga',
+        price: 35,
+        urlImage: 'https://fitnesswomen.es/cdn/shop/products/Sudadera-2023-03-15T122102.900.png?v=1678879308',
+        quantity: 1
+    },
+    {
+        id: 2,
+        title: 'Camiseta del madrid',
+        price: 30,
+        urlImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSvSiRqaeOWyo16uUjOhT-eUYHHVhBRg5DDVu-ixxVQA&s',
+        quantity: 1
+    },
+    {
+        id: 3,
+        title: 'Combo de Mochilas deportivas',
+        price: 150,
+        urlImage: 'https://degimnasio.net/wp-content/uploads/2018/10/Mochila-o-bolsa-deportiva.png.webp',
+        quantity: 1
+    },
+    {
+        id: 4,
+        title: 'Bicicleta',
+        price: 25,
+        urlImage: 'https://www.elsoldemexico.com.mx/doble-via/lnxm75-000_32bc2nm.jpg/ALTERNATES/LANDSCAPE_768/000_32BC2NM.jpg',
+        quantity: 1
+    }, {
+        id: 5,
+        title: 'Conjunto para Boxeo',
+        price: 20,
+        urlImage: 'https://www.tagoya.com/2948-thickbox_default/comjunto-boxeo-pantalon-camiseta.jpg',
+        quantity: 1
+    },
+
+    {
+        id: 6,
+        title: 'Mochila Deportiva Nike',
+        price: 50,
+        urlImage: 'https://www.mundodeportivo.com/elrecomendador/comparativas/wp-content/uploads/2023/03/Comparativa-mochilas-deportivas-3.jpg',
+        quantity: 1
+    },
+
+    {
+        id: 7,
+        title: 'Guantes de Boxeo',
+        price: 20,
+        urlImage: 'https://i5.walmartimages.com.mx/mg/gm/1p/images/product-images/img_large/00750042201576l.jpg?odnHeight=612&odnWidth=612&odnBg=FFFFFF',
+        quantity: 1
+    },
+
+    {
+        id: 8,
+        title: 'Casco para ciclismo',
+        price: 25,
+        urlImage: 'https://contents.mediadecathlon.com/p2179491/k$11e82e018019fc864cf1d2a532ab1f61/casco-bicicleta-urbana-500-amarillo-fluorescente.jpg?format=auto&quality=40&f=452x452',
+        quantity: 1
+    },
+
+    {
+        id: 9,
+        title: 'Balon de futbol',
+        price: 20,
+        urlImage: 'https://media.istockphoto.com/id/181412083/es/vector/f%C3%BAtbol-americano.jpg?s=612x612&w=0&k=20&c=IIQVma_gBQNZj20MvND2qWNg8L2RVNegTfhF53MbGKA=',
+        quantity: 1
+    },
+
+    {
+        id: 10,
+        title: 'Alfombra para yoga',
+        price: 10,
+        urlImage: 'https://cdnx.jumpseller.com/casa-contigo/image/28908748/alfombra-yoga-variedad-de-colores-plasticosmorija-D_NQ_NP_880092-MLC31872189114_082019-F-600x475.jpg?1666896153',
+        quantity: 1
+    }
+];

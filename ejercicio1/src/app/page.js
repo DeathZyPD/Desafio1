921e01ca2468070543636d63fa446ba7d49102dc
DeathZyPD <pablodagoberto2004@gmail.com>
2024-02-27 14:51:31 -0600

    ultimo cambio

--- ejercicio1/src/app/page.js
+++ ejercicio1/src/app/page.js
@@ -1,95 +1,101 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import React, { useState } from 'react';
+import styles from './page.module.css'; // Importa los estilos CSS utilizando CSS Modules
 
-export default function Home() {
+const peliculas = [
+  { nombre: 'Avatar', precio: 12.99 },
+  { nombre: 'El Señor de los Anillos: El Retorno del Rey', precio: 9.99},
+  { nombre: 'Inception', precio: 14.95 },
+  { nombre: 'Harry Potter y la Piedra Filosofal', precio: 8.99 },
+  { nombre: 'El espanta tiburones. Pelicula.', precio: 11.98 },
+  { nombre: 'Up', precio: 10.25 },
+  { nombre: 'The Hangover', precio:7.99 },
+  { nombre: 'The Dark Knight', precio: 11.49 },
+  { nombre: 'La llorona.', precio: 17.96 }, 
+  { nombre: 'Anabelle', precio: 10.91 },
+  { nombre: 'The Social Network', precio: 9.95 },
+  { nombre: 'Iron Man', precio: 13.75 },
+  
+];
+
+function App() {
+  const [selectedMovie, setSelectedMovie] = useState('');
+  const [compras, setCompras] = useState([]);
+
+  const agregarCompra = () => {
+    const peliculaSeleccionada = peliculas.find(pelicula => pelicula.nombre === selectedMovie);
+    if (peliculaSeleccionada) {
+      const nuevaCompra = { nombre: peliculaSeleccionada.nombre, precio: peliculaSeleccionada.precio, cantidad: 1 };
+      setCompras([...compras, nuevaCompra]);
+    }
+  };
+
+  const eliminarCompra = (index) => {
+    const nuevasCompras = [...compras];
+    nuevasCompras.splice(index, 1);
+    setCompras(nuevasCompras);
+  };
+
+  const disminuirCantidad = (index) => {
+    const nuevasCompras = [...compras];
+    if (nuevasCompras[index].cantidad > 1) {
+      nuevasCompras[index].cantidad--;
+      setCompras(nuevasCompras);
+    }
+  };
+
+  const aumentarCantidad = (index) => {
+    const nuevasCompras = [...compras];
+    nuevasCompras[index].cantidad++;
+    setCompras(nuevasCompras);
+  };
+
+  const calcularTotal = () => {
+    return compras.reduce((total, compra) => total + (compra.precio * compra.cantidad), 0).toFixed(2);
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    <div className={styles.container}>
+      <div className={styles.caja}>
+      <h1 className={styles.h1}>Lista de peliculas</h1>
+
+<div id="picker">
+  <select className={styles.cajas} onChange={(e) => setSelectedMovie(e.target.value)}>
+    <option value="">Selecciona la pelicula que deseas adquirir</option>
+    {peliculas.map((pelicula, index) => (
+      <option key={index} value={pelicula.nombre}>{pelicula.nombre}</option>
+    ))}
+  </select>
+  <button className={styles.agregarbtn} onClick={agregarCompra}>Agregar</button>
+</div>
+
+<div id="lista">
+<h1 className={styles.h1}>Lista de tus Compras</h1>
+  <ul >
+    {compras.map((compra, index) => (
+      <li  key={index} className={styles.lista}>
+        <div className={styles.infoPelicula}>
+<span className={styles.nombre}>{compra.nombre}</span>
+<span className={styles.precio}>${compra.precio.toFixed(2)}</span>
+</div>
+        <div id="cantidad" className={styles.btn}>
+          <button className={styles.btnCantidad} onClick={() => disminuirCantidad(index)}>-</button>
+          <input className={styles.inputCantidad} type="text" value={compra.cantidad} readOnly />
+          <button className={styles.btnCantidad} onClick={() => aumentarCantidad(index)}>+</button>
+          
         </div>
+        <button className={styles.eliminarbtn} onClick={() => eliminarCompra(index)}>Eliminar</button>
+      </li>
+      
+    ))}
+  </ul>
+  <p className={styles.total}>Total a pagar: ${calcularTotal()}</p>
+</div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+     
+    </div>
   );
 }
+
+export default App;
